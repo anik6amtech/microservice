95f30140b9e8d31e248c9d3598b0367b47fa6483
anik6amtech <anik.6amtech@gmmail.com>
2024-03-04 17:10:01 +0600

    user Managhement

---
+++ UserService/app/Http/Controllers/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function register (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|min:6',
+        ]);
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+
+        $request['password']=Hash::make($request['password']);
+        $request['remember_token'] = str::random(10);
+        $user = User::create($request->toArray());
+        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+        $response = ['token' => $token];
+        return response($response, 200);
+
+    }
+
+    public function login (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = ['token' => $token];
+                return response($response, 200);
+            } else {
+                $response = ["message" => "Password mismatch"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" =>'User does not exist'];
+            return response($response, 422);
+        }
+    }
+    public function logout (Request $request) {
+        $token = $request->user()->token();
+        $token->revoke();
+        $response = ['message' => 'You have been successfully logged out!'];
+        return response($response, 200);
+    }
+
+    public function userDetails(Request $request)
+    {
+        $user = $request->user();
+
+        if ($user) {
+            $userDetails = [
+                'name' => $user->name,
+                'email' => $user->email,
+                // Add other user details as needed
+            ];
+
+            return response(['user' => $userDetails], 200);
+        } else {
+            $response = ["message" => 'User not found'];
+            return response($response, 404);
+        }
+    }
+}
--- UserService/routes/api.php
+++ UserService/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+    // public routes
+    Route::post('/login', [AuthController::class,'login'])->name('login.api');
+    Route::post('/register',[AuthController::class,'register'])->name('register.api');
+    Route::post('/logout', [AuthController::class,'logout'])->name('logout.api');
+    Route::get('/userdetails', [AuthController::class,'userDetails'])->middleware('auth:api');
+
+    // ...
+
